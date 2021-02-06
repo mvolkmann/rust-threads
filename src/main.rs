b898c9bcec0b47094bf51d10b34c4e0ef7e3b72e
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-02-06 17:18:40 -0600

    added use of channels

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,48 @@
-// Rng stands for "random number generator".
-use rand::Rng;
+use rand::Rng; // stands for "random number generator".
+use std::sync::mpsc; // stands for "multiple producer, single consumer"
 use std::thread;
 use std::time::Duration;
 
-//type GenericError = Box<dyn std::error::Error + Send + Sync>;
-
 fn main() {
+    let (tx, rx) = mpsc::channel();
+
     const N: i32 = 3;
 
     // Start N threads and collect their handles.
     let mut handles = Vec::new();
     let mut rng = rand::thread_rng();
     for i in 1..=N {
-        // The "move" keyword below is necessary
-        // for the closure to take ownership of i.
-        // The next gives the rather scary error:
-        // `std::rc::Rc<std::cell::UnsafeCell<rand::rngs::adapter::ReseedingRng
-        // <rand_chacha::chacha::ChaCha12Core, rand::rngs::OsRng>>>`
-        // cannot be sent between threads safely
-        handles.push(thread::spawn(move || {
-            println!("thread {} started", i);
-            // Generate a random number of milliseconds to sleep.
-            let ms = rng.gen_range(500..5000);
-            thread::sleep(Duration::from_millis(ms));
-            println!("thread {} finished", i);
-            (i, ms) // thread number and milliseconds slept
-        }));
+        let ms = rng.gen_range(500..5000);
+        // The "move" keyword below is necessary for
+        // the closure to take ownership of i and ms.
+        let tx_clone = mpsc::Sender::clone(&tx);
+        handles.push(thread::spawn(
+            move || -> Result<(i32, u64), mpsc::SendError<_>> {
+                println!("thread {} started", i);
+                // Generate a random number of milliseconds to sleep.
+                thread::sleep(Duration::from_millis(ms));
+                let msg = format!("tx from thread {}!", i);
+                tx_clone.send(msg)?;
+                println!("thread {} finished", i);
+                Ok((i, ms)) // thread number and milliseconds slept
+            },
+        ));
     }
 
+    // Listen for channel messages in a different thread
+    // because the loop for receiving them never exits.
+    thread::spawn(move || {
+        for msg in rx {
+            println!("received {}", msg);
+        }
+    });
+
     // Wait for all the threads to finish.
+    // Results will be processed in the order the threads were created,
+    // not in the order in which the threads complete.
     for handle in handles {
         match handle.join() {
-            Ok(result) => println!("result = {:?}", result),
+            Ok(result) => println!("result = {:?}", result.unwrap()),
             Err(e) => eprintln!("error: {:?}", e),
         }
     }
